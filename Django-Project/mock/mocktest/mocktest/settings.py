038d3a885f033718ef94d7fa07093f8b46e5bd06
Debabrata <dsamanta1991@gmail.com>
2023-03-01 17:39:54 +0530

    removed

--- Django-Project/mock/mocktest/mocktest/settings.py
+++ Django-Project/mock/mocktest/mocktest/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7%lf@*o!anyc1br2w&%loew5hs$uf%8sk=k1b_fc+azqy15f)s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
